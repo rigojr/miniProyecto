082f006a09cde6b4b1b63e796c7ef56026368ed4
rigojr <rigojunnior@gmail.com>
2018-10-27 09:27:25 -0400

    Creando el activity shopping list

---
+++ app/src/main/java/android/example/com/ammunation/ShoppingList.java
@@ -0,0 +1,13 @@
+package android.example.com.ammunation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShoppingList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list);
+    }
+}
